cac005be5189b0326536c0b5a10207b303341292
dabrown104 <raven122685@yahoo.com>
2016-06-14 22:26:03 -0400

    Seminar 5 Assignment
    This is the assignment for seminar 5

---
+++ Seminar5App/Seminar5App/Views/DetailPage.xaml.cs
@@ -0,0 +1,16 @@
+using Seminar5App.ViewModels;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Controls;
+
+namespace Seminar5App.Views
+{
+    public sealed partial class DetailPage : Page
+    {
+        public DetailPage()
+        {
+            InitializeComponent();
+            NavigationCacheMode = NavigationCacheMode.Disabled;
+        }
+    }
+}
+
---
+++ Seminar5App/Seminar5App/Views/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+using System;
+using Seminar5App.ViewModels;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+using System.Collections.ObjectModel;
+
+namespace Seminar5App.Views
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            NavigationCacheMode = Windows.UI.Xaml.Navigation.NavigationCacheMode.Enabled;
+        }
+    }
+}
